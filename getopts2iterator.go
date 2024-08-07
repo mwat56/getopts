baba2e4f5cbb1923ccaf78618800140840d8fbb3
Matthias Watermann <support@mwat.de>
2024-08-07 22:06:54 +0200

    + modify `tIterator` struct to embed a `tExpectedOpts` instance to avoid more global variables; * change `newIterator()` to (re-)use the global singleton `gIterator` instance; + implement validity check in `Next()`; + implement `setPattern()` method to assign the option pattern to use; *update doc-comments and unit-tests;

--- getopts2iterator.go
+++ getopts2iterator.go
@@ -7,72 +7,91 @@ Copyright © 2024  M.Watermann, 10247 Berlin, Germany
 
 package getopts
 
-import (
-	"slices"
-)
-
 //lint:file-ignore ST1017 - I prefer Yoda conditions
 
 type (
-	// A list of options
-	tOptList []tOpt
-
 	// `tIterator` is a struct holding the actual commandline
-	// options and the current iteration state.
+	// options as well as the current iteration state.
 	tIterator struct {
-		args  tArgList // Slice of option arguments
-		opts  tOptList // Slice of option names of the map for iteration
-		index int      // Current index for iteration
+		// List of option arguments
+		optArgs *tOptArgList
+
+		// List of known/expected options and argument requirement
+		expected *tExpectedOpts
+
+		// Current index for iteration:
+		index int
 	}
 )
 
 // --------------------------------------------------------------------
-// Constructor function
+// tIterator constructor
 
-// `newIterator()` initialises a `tArgIterator` with the provided
-// map of option arguments.
+// `newIterator()` initialises a `tIterator` with the provided map
+// of option arguments.
+//
+// This function treats the internal global variable `gIterator` as a global
+// singleton, i.e. only the very first call to this function actually creates
+// a new instance while all following calls simply update the global variable
+// according to the provided `tOptArgMap` instance.
 //
 // Parameters:
 //   - `aMap`: The list of commandline options and their respective argument.
 //
 // Returns:
-//   - `*tArgIterator`: The new iterator for `aMap`.
-func newIterator(aMap tArgList) *tIterator {
-	options := make(tOptList, 0, len(aMap))
-	for o := range aMap {
-		options = append(options, o)
-	}
-	if 1 < len(options) {
-		// Sort the slice:
-		slices.Sort(options)
+//   - `*tIterator`: The iterator for `aList`.
+func newIterator(aList *tOptArgList) *tIterator {
+	if nil == gIterator {
+		gIterator = &tIterator{
+			optArgs: aList,
+			// leave `expected` for lazy initialisation
+			index: 0,
+		}
+	} else {
+		gIterator.optArgs = aList
 	}
 
-	return &tIterator{
-		args:  aMap,
-		opts:  options,
-		index: 0,
-	}
-} // newOptIterator()
+	return gIterator
+} // newIterator()
 
 // --------------------------------------------------------------------
-// tOptIterator methods
+// tIterator methods
 
 // `Next()` returns the next key-value pair in the iteration.
 // It returns `false` if there are no more items.
 //
 // Returns:
 //   - `rOpt`: The current option in the iteration.
-//   - `rArg`: The current option's argument in the iteration.
-//   - `rOK`: Indicator for whether there are more options to iterate over.
-func (oi *tIterator) Next() (rOpt tOpt, rArg TArg, rOK bool) {
-	if oi.index < len(oi.opts) {
-		rOpt = oi.opts[oi.index]
-		rArg = oi.args[rOpt]
+//   - `rArg`: The current option's argument.
+//   - `rMore`: Indicator for whether there are more options to process.
+func (oi *tIterator) Next() (rOpt tOpt, rArg TArg, rMore bool) {
+	if oi.index < len(*oi.optArgs) {
+		oa := (*oi.optArgs)[oi.index]
+		rOpt = oa.opt
+		rArg = oa.arg
 		oi.index++
-		rOK = true
-	} else {
-		rOK = false
-		// leave the other return values at their default
+		rMore = (oi.index < len(*oi.optArgs))
+		//} else {	rMore = false
+		// leave the other return values at their zero values
+	}
+
+	// if nil == oi.expected {
+	// 	// This should never happen because this singleton
+	// 	// instance actually IS the global iterator ...
+	// 	oi.expected = gIterator.expected
+	// }
+
+	if !oi.expected.isValid(rOpt, rArg) {
+		if rMore {
+			// An unknown option or one without the required
+			// argument is simply ignored and we continue by
+			// going through the next iteration:
+			rOpt, rArg, rMore = oi.Next()
+		} else {
+			rOpt = tOpt("")
+			rArg = TArg("")
+			// `rMore` remains false
+		}
 	}
 
 	return
@@ -86,12 +105,50 @@ func (oi *tIterator) Reset() {
 	oi.index = 0
 } // Reset()
 
+// `setPattern()` sets up the options pattern for the options iterator.
+//
+// The method sets the pattern for the iterator, which is used to
+// determine the expected options and their required arguments. If this
+// is the very first call, it initialises the private list of expected
+// options (lazy initialisation). If there is already an existing
+// pattern, the method ensures that the pattern is up-to-date.
+//
+// Note, that for the iterator to work properly, this method must be called
+// at least once (usually by the `Getopts()` function) prior to the [Next]
+// method. This requirement is satisfied by the `init()` function which
+// passes the options pattern `h|-help` that is commonly used IRL. The
+// actual options pattern used by `Getopts()` is passed by [Get] to
+// method. So both, by default and by setting it explicitly, this method
+// will be called internally, so there's no need to expose it publicly.
+//
+// Every call to this method will reset the iterator's index if the
+// given options pattern is different from the one previously used.
+//
+// Parameters:
+//   - `aPattern`: The new pattern to be used by the iterator.
+//
+// Returns:
+//   - `*tIterator`: The iterator instance with the updated pattern.
+func (oi *tIterator) setPattern(aPattern string) *tIterator {
+	if nil == oi.expected {
+		// This seems to be the very first call, hence we
+		// need to initialise the list of expected options:
+		oi.expected = newExpectedOpts(aPattern)
+	} else if oi.expected.previous != aPattern {
+		// Make sure the pattern is up-to-date:
+		oi.expected = oi.expected.parse(aPattern)
+		oi.index = 0
+	}
+
+	return oi
+} // setPattern()
+
 // --------------------------------------------------------------------
 
 var (
-	// Global/internal instance of the iterator to make it
-	// accessible for the public `getopts` function. It is
-	// initialised automatically by getopts' `init()` function.
+	// Internal instance of the iterator to make it accessible for the
+	// public `Get()` function. It is initialised automatically by
+	// the getopts' `realInit()` function.
 	gIterator *tIterator
 )
 
--- getopts2iterator_test.go
+++ getopts2iterator_test.go
@@ -14,18 +14,37 @@ import (
 
 //lint:file-ignore ST1017 - I prefer Yoda conditions
 
-func Test_newOptIterator(t *testing.T) {
+func prep4Test() *tOptArgList {
+	// if gSomeTestsAreRunning {
+	// 	cmdLineArgs := []string{
+	// 		`appname`,
+	// 		`-a`,
+	// 		`-i`,
+	// 		`--infile`,
+	// 		`config.in`,
+	// 		`--help`,
+	// 	}
+	// 	realInit(cmdLineArgs)
+	// 	return newOptArgMap(cmdLineArgs)
+	// }
+	return gIterator.optArgs
+} // prep4Test()
+
+func Test_newIterator(t *testing.T) {
+	l1 := prep4Test()
+	w1 := gIterator
 
 	tests := []struct {
 		name string
-		amap tArgList
+		list *tOptArgList
 		want *tIterator
 	}{
+		{"1", l1, w1},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := newIterator(tt.amap); !reflect.DeepEqual(got, tt.want) {
+			if got := newIterator(tt.list); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("%q: newOptIterator() = %v, want %v",
 					tt.name, got, tt.want)
 			}
@@ -33,52 +52,154 @@ func Test_newOptIterator(t *testing.T) {
 	}
 } // TestNewOptIterator()
 
-func Test_tOptIterator_Next(t *testing.T) {
+func Test_tIterator_Next(t *testing.T) {
+	// Set getopts `init()` :: []string{
+	// 	"testingApplication",
+	// 	`-a`, // Flag option
+	// 	`-i`, // Error: expected with argument => skipped
+	// 	`--infile`, `config.in`,
+	// 	`--help`, // Flag option
+	// }
+	a1 := prep4Test()
+	p1 := "|a|i:|-infile:|-help"
+	i1 := newIterator(a1).setPattern(p1)
+	o1, r1, m1 := tOpt("a"), TArg(""), true
+
+	// "-i" is skipped because of its missing argument
+	i2 := i1
+	o2, r2, m2 := tOpt("-infile"), TArg("config.in"), true
+
+	i3 := i2
+	o3, r3, m3 := tOpt("-help"), TArg(""), false
 
 	tests := []struct {
 		name     string
 		oi       *tIterator
-		wantROpt tOpt
-		wantRArg TArg
-		wantROK  bool
+		wantOpt  tOpt
+		wantArg  TArg
+		wantMore bool
 	}{
+		{"1", i1, o1, r1, m1},
+		{"2", i2, o2, r2, m2},
+		{"3", i3, o3, r3, m3},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotROpt, gotRArg, gotROK := tt.oi.Next()
-			if gotROpt != tt.wantROpt {
-				t.Errorf("%q: tOptIterator.Next() gotROpt = %v, want %v",
-					tt.name, gotROpt, tt.wantROpt)
+			gotOpt, gotArg, gotMore := tt.oi.Next()
+			if gotOpt != tt.wantOpt {
+				t.Errorf("%q: tIterator.Next() gotOpt = %q, want %q;\npattern %q",
+					tt.name, gotOpt, tt.wantOpt, tt.oi.expected.previous)
 			}
-			if gotRArg != tt.wantRArg {
-				t.Errorf("%q: tOptIterator.Next() gotRArg = %v, want %v",
-					tt.name, gotRArg, tt.wantRArg)
+			if gotArg != tt.wantArg {
+				t.Errorf("%q: tIterator.Next() gotArg = %q, want %q;\npattern %q",
+					tt.name, gotArg, tt.wantArg, tt.oi.expected.previous)
 			}
-			if gotROK != tt.wantROK {
-				t.Errorf("%q: tOptIterator.Next() gotROK = %v, want %v",
-					tt.name, gotROK, tt.wantROK)
+			if gotMore != tt.wantMore {
+				t.Errorf("%q: tIterator.Next() gotMore = %t, want %t;\npattern %q",
+					tt.name, gotMore, tt.wantMore, tt.oi.expected.previous)
 			}
+
 		})
 	}
-} // Test_tOptIterator_Next()
+} // Test_tIterator_Next()
 
-func Test_tOptIterator_Reset(t *testing.T) {
+func Test_tIterator_Reset(t *testing.T) {
+	prep4Test()
+
+	oal0 := &tOptArgList{}
+	i0 := newIterator(oal0)
 
 	tests := []struct {
 		name string
 		oi   *tIterator
 	}{
+		{"0", i0},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.oi.Reset()
 			if 0 != tt.oi.index {
-				t.Errorf("%q: tOptIteratorReset.() gotROK = %v, want %v",
+				t.Errorf("%q: tIteratorReset.() gotROK = %v, want %v",
 					tt.name, tt.oi.index, 0)
 
 			}
 		})
 	}
-} // Test_tOptIterator_Reset()
+} // Test_tIterator_Reset()
+
+func Test_tIterator_setPattern(t *testing.T) {
+	prep4Test()
+
+	oal0 := &tOptArgList{}
+	p0 := "h|-help"
+	e0 := &tExpectedOpts{previous: p0}
+	i0 := newIterator(oal0)
+	w0 := &tIterator{
+		optArgs:  oal0,
+		expected: e0,
+	}
+
+	oal1 := &tOptArgList{}
+	p1 := "a|b:|c:|d"
+	e1 := &tExpectedOpts{previous: p1}
+	i1 := newIterator(oal1)
+	w1 := &tIterator{
+		optArgs:  oal1,
+		expected: e1,
+	}
+
+	c2 := []string{
+		`appname`,
+		`-a`,
+		`-b`, // without the required argument
+		`--infile`, `config.in`,
+		`--help`,
+	}
+	oal2 := newOptArgList(c2)
+	p2 := "||a|b:|c:||d||"
+	i2 := newIterator(oal2)
+	w2 := newIterator(oal2)
+
+	oal3 := oal2
+	p3 := "a|b:|:-infile|d"
+	i3 := newIterator(oal3)
+	w3 := newIterator(oal3)
+
+	tests := []struct {
+		name    string
+		iter    *tIterator
+		pattern string
+		want    *tIterator
+	}{
+		{"0", i0, p0, w0},
+		{"1", i1, p1, w1},
+		{"2", i2, p2, w2},
+		{"3", i3, p3, w3},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.iter.setPattern(tt.pattern)
+			if (nil != got) && (nil != tt.want) {
+				if nil != got.expected && nil != tt.want.expected {
+					if got.expected.previous != tt.want.expected.previous {
+						t.Errorf("%q 3: tIterator.setPattern() = %q, want %q",
+							tt.name, got.expected.previous, tt.want.expected.previous)
+					}
+				} else {
+					t.Errorf("%q 2: tIterator.setPattern() = %v, want %v",
+						tt.name, got.expected, tt.want.expected)
+				}
+			} else {
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("%q 1: tIterator.setPattern() = %v, want %v",
+						tt.name, got, tt.want)
+				}
+			}
+		})
+	}
+} // Test_tIterator_setPattern()
+
+/* _EoF_ */
