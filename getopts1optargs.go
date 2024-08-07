8ada34596df7377350f3c9bc73787dac7a734eb5
Matthias Watermann <support@mwat.de>
2024-08-07 22:32:03 +0200

    + implement `tOptArg` and `tOptArgList` type to replace the former map to keep the order of the options constant; + implement `Equal()` methods for better testing support; + implement ` newOptArgList()` constructor to set up a new instance; * update doc-comments and unit-tests;

--- getopts1optargs.go
+++ getopts1optargs.go
@@ -8,6 +8,7 @@ Copyright © 2024  M.Watermann, 10247 Berlin, Germany
 package getopts
 
 import (
+	"fmt"
 	"strconv"
 )
 
@@ -18,16 +19,27 @@ type (
 	TArg string
 
 	// `tOpt` represents a commandline option.
+	// Type definition provided for better readability and clarity.
 	tOpt string
 
-	// A map holding the _actual_ commandline options and their arguments.
-	tArgList map[tOpt]TArg
+	// `tOptarg` is a key/value pair of a single commandline option
+	// and its (potentially empty) argument.
+	tOptArg struct {
+		opt tOpt
+		arg TArg
+	}
+
+	// `tOptArgList` is a list of option/argument pairs.
+	//
+	// A slice holding the _actual_ commandline options with their argument.
+	// Type definition provided for better readability and clarity.
+	tOptArgList []tOptArg
 )
 
 // --------------------------------------------------------------------
 // TArg methods
 
-// `Bool()` returns an option's argument as a boolean value.
+// `Bool()` returns the argument's value as a boolean value.
 //
 // `0`, `f`, `F`, `n`, and `N` are considered `false` while
 // `1`, `t`, `T`, `y`, `Y`, `j`, `J`, `o`, `O` are considered `true`.
@@ -40,7 +52,7 @@ type (
 // mentioned above, then the method's result will be `false`.
 //
 // Returns:
-// - `bool`: The options's argument as a Boolean.
+// - `bool`: The argument's value as a Boolean.
 func (a TArg) Bool() bool {
 	if 0 < len(a) {
 		switch a[:1] {
@@ -55,7 +67,18 @@ func (a TArg) Bool() bool {
 	return false
 } // Bool()
 
-// `Float()` returns an option's argument as a 64bit floating point.
+// `Equal()` checks if this argument is equal to another one.
+//
+// Parameters:
+//   - `aArg`: The argument to compare with this one.
+//
+// Returns:
+//   - `bool`: An indicator for whether this argument equals `aArg`.
+func (a TArg) Equal(aArg TArg) bool {
+	return a == aArg
+} // Equal()
+
+// `Float()` returns the argument's value as a 64bit floating point.
 //
 // If the string is well-formed and near a valid floating point number,
 // [Float] returns the nearest floating point number rounded using
@@ -65,7 +88,7 @@ func (a TArg) Bool() bool {
 // will be the value `float64(0.0)`.
 //
 // Returns:
-// - `float64`: A options's argument as a 64bit floating point.
+// - `float64`: The argument's value as a 64bit floating point.
 func (a TArg) Float() float64 {
 	if "" == string(a) {
 		return float64(0.0)
@@ -79,16 +102,16 @@ func (a TArg) Float() float64 {
 	return float64(0.0)
 } // Float()
 
-// `Int()` returns an option's argument as an integer.
+// `Int()` returns the argument's value as an integer.
 //
-// In case the option can't be converted to an integer the method's result
-// will be the value `int(0)` (zero).
+// In case the option can't be converted to an integer the method's
+// result will be the value `int(0)` (zero).
 //
 // Returns:
-// - `int`: An option's argument as an integer.
+// - `int`: The argument's value as an integer.
 func (a TArg) Int() int {
 	if "" == string(a) {
-		return 0
+		return int(0)
 	}
 
 	if i64, err := strconv.ParseInt(string(a), 10, 0); nil == err {
@@ -98,10 +121,12 @@ func (a TArg) Int() int {
 	return int(0)
 } // Int()
 
-// `String()` returns an option's argument as a string.
+// `String()` returns a stringified version of the argument.
+//
+// Note: This is mainly for debugging purposes and has no real life use.
 //
 // Returns:
-// - `string`: An option's argument as a string.
+//   - `string`: The stringified version of the current argument.
 func (a TArg) String() string {
 	return string(a)
 } // String()
@@ -109,8 +134,180 @@ func (a TArg) String() string {
 // --------------------------------------------------------------------
 // tOpt methods
 
+// `Equal()` checks if this option is equal to another one.
+//
+// Parameters:
+//   - `aOpt`: The option to compare with this one.
+//
+// Returns:
+//   - `bool`: An indicator for whether this option equals `aOpt`.
+func (o tOpt) Equal(aOpt tOpt) bool {
+	return o == aOpt
+} // Equal()
+
+// `String()` returns a stringified version of the option.
+//
+// Note: This is mainly for debugging purposes and has no real life use.
+//
+// Returns:
+//   - `string`: The stringified version of the current option.
 func (o tOpt) String() string {
 	return string(o)
+} // String()
+
+// --------------------------------------------------------------------
+
+// `Equal()` checks if this option/argument is equal to another one.
+//
+// Parameters:
+//   - `aOpt`: The option/argument to compare with this one.
+//
+// Returns:
+//   - `bool`: Indicator for whether this option/argument equals `aOptArg`.
+func (oa tOptArg) Equal(aOptArg tOptArg) bool {
+	if oa.opt != aOptArg.opt {
+		return false
+	}
+	return oa.arg == aOptArg.arg
+} // Equal()
+
+// `String()` returns a stringified version of the option/argument.
+//
+// Note: This is mainly for debugging purposes and has no real life use.
+//
+// Returns:
+//   - `string`: The stringified version of the current option/argument.
+func (oa tOptArg) String() string {
+	return fmt.Sprintf("[%q: %q]", oa.opt, oa.arg)
+} // String()
+
+// --------------------------------------------------------------------
+// tOptArgList Constructor
+
+// `newOptArgList()` creates a new option/argument list based on `aArgList`.
+//
+// The function checks whether at least one argument is passed and then
+// initialises an options/arguments list to store the current commandline
+// options and arguments passed as `aArgList`. If the provided list of
+// commandline options is empty (apart from the name of the running
+// application), the function sets up the standard options "-h" and
+// "--help" with an empty argument (such making them a `flag option`).
+//
+// Note that only the options's name is stored, i.e. the leading hyphen used
+// on the actual commandline is removed. This means that so-called `short
+// options` will be only a single letter and the `long options` will have
+// their first hyphen removed. For example, the `short option` of "-h" will
+// be stored as "h", and the `long option` of "--help" will be stored as
+// "-help".
+//
+// Parameters:
+//   - `aArgList`: A list of commandline options and arguments.
+//
+// Returns:
+//   - `*tArgList`: A pointer to the newly created argument list.
+func newOptArgList(aArgList []string) *tOptArgList {
+	// Previously we used a map to store the key/value pairs. However,
+	// because a map can not keep it's assigned order option/argument
+	// pairs could only accessed in a random order. Hence we switched
+	// to using a slice instead to keep the order of the commandline
+	// options intact.
+	oal := make(tOptArgList, 0, len(aArgList))
+
+	if 1 >= len(aArgList) {
+		var empty TArg
+		// Set up some standard default options:
+		oal = append(oal, tOptArg{tOpt(`h`), empty})
+		oal = append(oal, tOptArg{tOpt(`-help`), empty})
+		// nothing more to do here:
+		return &oal
+	}
+
+	// Get the commandline arguments without the app's path/name
+	// but with an added (empty) argument to allow for a peek ahead
+	// without any range problems when we happen to process the
+	// very last real option or argument.
+	optList := append(aArgList[1:], "")
+
+	// Exclude the peek-ahead dummy added at the end:
+	oLen := len(optList) - 1
+
+	for i := 0; i < oLen; i++ {
+		o := optList[i]
+		if len(o) < 2 {
+			// We expect at least `-o` i.e. two characters.
+			continue
+		}
+		if '-' == o[0] {
+			// It's actually an option (not
+			// an unexpected argument)
+			o = o[1:]
+
+			//
+			//TODO: should we check for `--` as stdOut indicator?
+			//
+
+			p := optList[i+1] // peek ahead
+			// If there's another value that is not
+			// an argument, ignore it here:
+			if (0 < len(p)) && ('-' == p[0]) {
+				// leave it for the next loop step
+				p = ""
+			} else {
+				i++
+			}
+
+			// This might assign an empty argument to the option,
+			// a situation which will be handled by the iterator's
+			// `Next()` method by checking whether an argument is
+			// actually required according to the options pattern.
+			oal = append(oal, tOptArg{tOpt(o), TArg(p)})
+		}
+	}
+
+	return &oal
+} // newOptArgList()
+
+// --------------------------------------------------------------------
+// tOptArgList methods
+
+// `Equal()` checks if this option argument list is equal to another
+// one: the same length and all elements equal.
+//
+// If the lengths are different, [Equal] returns `false`. Otherwise, the
+// elements are compared in increasing index order, and the comparison
+// stops at the first unequal pair.
+//
+// Parameters:
+//   - `aList`: The slice to compare with this one.
+//
+// Returns:
+//   - `bool`: An indicator for whether this slice is equal to `aList`.
+func (oal tOptArgList) Equal(aList tOptArgList) bool {
+	if len(oal) != len(aList) {
+		return false
+	}
+
+	for idx, oa := range oal {
+		if !oa.Equal(aList[idx]) {
+			return false
+		}
+	}
+
+	return true
+} // Equal()
+
+// `String()` returns a stringified version of the option argument list
+//
+// Note: This is mainly for debugging purposes and has no real life use.
+//
+// Returns:
+//   - `string`: The stringified version of the current options list.
+func (oal tOptArgList) String() (rStr string) {
+	for _, oa := range oal {
+		rStr += fmt.Sprintf("%s\n", oa)
+	}
+
+	return
 } // String()
 
 /* _EoF_ */
--- getopts1optargs_test.go
+++ getopts1optargs_test.go
@@ -7,9 +7,102 @@ Copyright © 2024  M.Watermann, 10247 Berlin, Germany
 
 package getopts
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 //lint:file-ignore ST1017 - I prefer Yoda conditions
+
+func Test_newOptArgList(t *testing.T) {
+	/* This is the list set up by `init()` for testing purposes
+	args = []string{
+		"testingApplication",
+		`-a`, // Flag option
+		`-i`, // Error: intended with argument => ignored
+		`--infile`, `config.in`,
+		`--help`, // Flag option
+	}
+	*/
+	var empty TArg
+	a1 := []string{
+		"testingApplication",
+		`-a`, // Flag option
+		`-i`, // Error: intended with argument => ignored
+		`--infile`, `config.in`,
+		`--help`, // Flag option
+	}
+	w1 := make(tOptArgList, 0, 4)
+	w1 = append(w1, tOptArg{tOpt(`a`), empty})
+	w1 = append(w1, tOptArg{tOpt(`i`), empty})
+	w1 = append(w1, tOptArg{tOpt(`-infile`), TArg(`config.in`)})
+	w1 = append(w1, tOptArg{tOpt(`-help`), empty})
+
+	tests := []struct {
+		name string
+		args []string
+		want *tOptArgList
+	}{
+		{"1", a1, &w1},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newOptArgList(tt.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("%q: newOptArgList() =\n%v\n want \n%v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_newOptArgList()
+
+func Test_tOptArgList_Equal(t *testing.T) {
+	oal1 := tOptArgList{}
+	woa1 := tOptArgList{}
+	oal2 := tOptArgList{tOptArg{"o2", "a2"}}
+	woa2 := tOptArgList{tOptArg{"wo2", "wa2"}}
+	oal3 := oal1
+	woa3 := tOptArgList{tOptArg{"wo3", "wa3"}}
+
+	tests := []struct {
+		name  string
+		oal   tOptArgList
+		other tOptArgList
+		want  bool
+	}{
+		{"1", oal1, woa1, true},
+		{"2", oal2, woa2, false},
+		{"3", oal3, woa3, false},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.oal.Equal(tt.other); got != tt.want {
+				t.Errorf("%q: tOptArgList.Equal() = %v, want %v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tOptArgList_Equal()
+
+func Test_tOptArgList_String(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		oal      tOptArgList
+		wantRStr string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRStr := tt.oal.String(); gotRStr != tt.wantRStr {
+				t.Errorf("%q: tOptArgList.String() = %q, want %q",
+					tt.name, gotRStr, tt.wantRStr)
+			}
+		})
+	}
+} // Test_tOptArgList_String()
 
 func TestTArg_Bool(t *testing.T) {
 
@@ -69,6 +162,7 @@ func TestTArg_Int(t *testing.T) {
 } // TestTArg_Int()
 
 func TestTArg_String(t *testing.T) {
+
 	tests := []struct {
 		name string
 		a    TArg
@@ -85,3 +179,5 @@ func TestTArg_String(t *testing.T) {
 		})
 	}
 } // TestTArg_String()
+
+/* _EoF_ */
