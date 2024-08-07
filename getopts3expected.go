57ff8eee8e21e64e4aad9c1cd05369cd9b8f5b2e
Matthias Watermann <support@mwat.de>
2024-08-07 21:47:15 +0200

    * modify `newExpectedOpts()` to basically return a singleton: * modify `parse()` to remove unnecessary garbage from option definitions; - remove obsoleted global `gExpectedOpts` variable; + update unit-tests;

--- getopts3expected.go
+++ getopts3expected.go
@@ -8,19 +8,18 @@ Copyright © 2024  M.Watermann, 10247 Berlin, Germany
 package getopts
 
 import (
-	"slices"
 	"strings"
 )
 
 //lint:file-ignore ST1017 - I prefer Yoda conditions
 
 type (
-
 	// A map of the _expected_ options indicating whether
 	// it needs an argument to work.
 	tArgBool map[tOpt]bool
 
-	tExpectedArgs struct {
+	// A map of the known/expected options and argument requirement
+	tExpectedOpts struct {
 		// List of all _expected_ commandline options
 		argBool tArgBool
 
@@ -29,66 +28,146 @@ type (
 	}
 )
 
-func newExpectedArgs() *tExpectedArgs {
-	return &tExpectedArgs{
-		argBool: make(tArgBool),
-	}
-} // newExpectedArgs()
-
-// `needArgument()` checks if a given commandline option requires an argument.
+// `newExpectedOpts()` sets up a instance of `tExpectedOpts`.
 //
-// This function takes a command-line option name as input and returns
+// This function is a first step to manage the expected commandline
+// options and their respective arguments.
+//
+// Parameters:
+//   - `aPattern`: The pattern declaring which commandline options to expect.
+//
+// Returns:
+//   - `*tExpectedOpts`: The requested `tExpectedArgs` instance.
+func newExpectedOpts(aPattern string) *tExpectedOpts {
+	var eo *tExpectedOpts
+
+	if nil == gIterator {
+		eo = &tExpectedOpts{
+			argBool: make(tArgBool),
+		}
+	} else {
+		if nil == gIterator.expected {
+			eo = &tExpectedOpts{
+				argBool: make(tArgBool),
+			}
+			gIterator.expected = eo.parse(aPattern)
+
+			return eo
+		}
+
+		eo = gIterator.expected
+		if eo.previous == aPattern {
+			return eo
+		}
+	}
+
+	return eo.parse(aPattern)
+} // newExpectedOpts()
+
+// `isValid()` checks if a given commandline option is recognised.
+//
+// This function takes a commandline option name as input and returns
 // a boolean value indicating whether the option requires an argument.
 //
 // Parameters:
 //   - `aOpt`: The commandline option name to check.
+//   - `rArg`: The option's argument to check.
 //
 // Returns:
-//   - `bool`: Indicator for whether the option requires an argument.
-func (ea tExpectedArgs) needArgument(aOpt tOpt) bool {
-	result, ok := ea.argBool[aOpt]
-	if !ok {
+//   - `bool`: Indicator for whether the option is recognised.
+func (eo tExpectedOpts) isValid(aOpt tOpt, rArg TArg) bool {
+	needArg, valid := eo.argBool[aOpt]
+	if needArg && ("" == string(rArg)) {
 		return false
 	}
 
-	return result
-} // needArgument()
+	return valid
+} // isValid()
 
 // `parse()` parses the provided pattern and updates the expected arguments.
+//
+// NOTE: If the given `aPattern` is empty, then the pattern `h|-help` will
+// be used which usually triggers a help request and the termination of
+// the running application.
 //
 // Parameters:
 //   - `aPattern`: The pattern declaring which commandline options to expect.
 //
 // Returns:
 //   - `*tExpectedArgs`: The updated expected arguments.
-func (ea *tExpectedArgs) parse(aPattern string) *tExpectedArgs {
-	if aPattern == ea.previous {
-		return ea
+func (eo *tExpectedOpts) parse(aPattern string) *tExpectedOpts {
+	if "" == aPattern {
+		aPattern = "h|-help"
 	}
 
-	// Split the pattern string by `|` into a slice of option names
-	opts := strings.Split(aPattern, `|`)
-	slices.Sort(opts)
+	// Shortcut to avoid unnecessary parsing:
+	if aPattern == eo.previous {
+		return eo
+	}
 
-	oLen := len(opts)
+	// Reset the map to remove all previous entries
+	clear(eo.argBool)
+
+	// Split the pattern string by `|` into a slice of options
+	// and their arguments:
+	optargs := strings.Split(aPattern, `|`)
+	// The order of the options reflects the order in the given
+	// pattern, but not order of options on the commandline.
+
+	oLen := len(optargs)
 	for i := 0; i < oLen; i++ {
-		opt := opts[i]
-		opt = string(opt[len(opt)-1])
-		if ":" == opt {
-			o := tOpt(opt[:len(opt)-1])
-			ea.argBool[o] = true
-		} else {
-			ea.argBool[tOpt(opt)] = false
+		opt := optargs[i]
+		if "" == opt {
+			// leading/trailing or orphaned pipe separator
+			continue
 		}
+
+		// Look for leading colons and spaces to determine whether
+		// to remove leasing garbage:
+		pos, optL := 0, len(opt)
+		for (pos < optL) && ((`:` == string(opt[pos])) || (` ` == string(opt[pos]))) {
+			pos++
+		}
+		if 0 < pos {
+			if pos == optL {
+				continue // ignore empty option
+			}
+			opt = opt[pos:]
+			optL = len(opt)
+		}
+
+		// Now, look for trailing colons and spaces to determine whether
+		// the option requires an argument:
+		pos = optL
+		needArg := false
+	argLoop:
+		for 0 < pos {
+			// for (0 < pos) && ((`:` == string(opt[pos-1])) || (` ` == string(opt[pos-1]))) {
+			switch string(opt[pos-1]) {
+			case `:`:
+				needArg = true
+				pos--
+
+			case ` `:
+				pos--
+
+			default:
+				break argLoop
+			}
+		}
+
+		if needArg || (pos < optL) {
+			if 0 == pos {
+				continue // ignore empty option
+			}
+			opt = opt[:pos]
+		}
+		eo.argBool[tOpt(opt)] = needArg
 	}
-	ea.previous = aPattern
+	// Save the pattern for a possible future call:
+	eo.previous = aPattern
 
-	return ea
+	return eo
 } // parse()
-
-var (
-	// global/private instance of `tExpectedArgs`
-	gExpectedOpts *tExpectedArgs
-)
 
 /* _EoF_ */
--- getopts3expected_test.go
+++ getopts3expected_test.go
@@ -8,65 +8,126 @@ Copyright © 2024  M.Watermann, 10247 Berlin, Germany
 package getopts
 
 import (
-	"reflect"
 	"testing"
 )
 
 //lint:file-ignore ST1017 - I prefer Yoda conditions
 
 func Test_newExpectedArgs(t *testing.T) {
+	p1 := `a| b| c | :d | e: | f |`
+	w1 := &tExpectedOpts{previous: p1}
+
 	tests := []struct {
-		name string
-		want *tExpectedArgs
+		name    string
+		pattern string
+		want    *tExpectedOpts
 	}{
+		{"1", p1, w1},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := newExpectedArgs(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("%q: newExpectedArgs() = %v, want %v",
-					tt.name, got, tt.want)
+			got := newExpectedOpts(tt.pattern)
+			if //!reflect.DeepEqual(got, tt.want) {
+			got.previous != tt.want.previous {
+				t.Errorf("%q: newExpectedArgs() =\n%v\n want \n%v",
+					tt.name, got.previous, tt.want.previous)
 			}
 		})
 	}
 } // Test_newExpectedArgs()
 
-func Test_tExpectedArgs_needArgument(t *testing.T) {
+func Test_tExpectedOpts_isValid(t *testing.T) {
+	/* used by `getopts.init()` in testing/debugging mode:
+	args = []string{
+		"testingApplication",
+		`-a`, // Flag option
+		`-i`, // Error: intended with argument => ignored
+		`--infile`, `config.in`,
+		`--help`, // Flag option
+	}
+	*/
+	eo := newExpectedOpts(`|a|i:|-infile:|-help|`)
+
+	o0, a0, w0 := tOpt("0"), TArg("0"), false
+	o1, a1, w1 := tOpt("a"), TArg(""), true
+	o2, a2, w2 := tOpt("i"), TArg(""), false
+	o3, a3, w3 := tOpt("-infile"), TArg("dummy.txt"), true
 
 	tests := []struct {
-		name string
-		ea   tExpectedArgs
-		opt  tOpt
-		want bool
+		name      string
+		opt       tOpt
+		arg       TArg
+		wantValid bool
 	}{
+		{"0", o0, a0, w0},
+		{"1", o1, a1, w1},
+		{"2", o2, a2, w2},
+		{"3", o3, a3, w3},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.ea.needArgument(tt.opt); got != tt.want {
-				t.Errorf("%q: tExpectedArgs.argNeeded() = %v, want %v",
-					tt.name, got, tt.want)
+			gotValid := eo.isValid(tt.opt, tt.arg)
+			if gotValid != tt.wantValid {
+				t.Errorf("%q: tExpectedOpts.isValid() gotValid = %v, want %v",
+					tt.name, gotValid, tt.wantValid)
 			}
 		})
 	}
-} // Test_tExpectedArgs_needArgument()
+} // Test_tExpectedOpts_isValid()
 
 func Test_tExpectedArgs_parse(t *testing.T) {
+	/* used by `getopts.init()` in testing/debugging mode:
+	args = []string{
+		"testingApplication",
+		`-a`, // Flag option
+		`-i`, // Error: intended with argument => ignored
+		`--infile`, `config.in`,
+		`--help`, // Flag option
+	}
+	*/
+
+	p1 := `a|i:|-infile:|-help`
+	e1 := newExpectedOpts(p1)
+	p2 := `|a|i:|-k|-help|`
+	e2 := newExpectedOpts(p2)
+	p3 := `a|i::|-j:|-zzz`
+	e3 := newExpectedOpts(p3)
+	p4 := `a|i::|::j|-zzz`
+	e4 := newExpectedOpts(p4)
+	p5 := `a|i::|::|::m|z`
+	e5 := newExpectedOpts(p5)
+	p6 := `a|i::|j|::k::|z`
+	e6 := newExpectedOpts(p6)
+	p7 := `a | i:: | j | ::k:: | z`
+	e7 := newExpectedOpts(p7)
+	p8 := `a | i : | j | : k : | z`
+	e8 := newExpectedOpts(p8)
 
 	tests := []struct {
 		name    string
-		ea      *tExpectedArgs
+		eo      *tExpectedOpts
 		pattern string
-		want    *tExpectedArgs
 	}{
+		{"1", e1, p1},
+		{"2", e2, p2},
+		{"3", e3, p3},
+		{"4", e4, p4},
+		{"5", e5, p5},
+		{"6", e6, p6},
+		{"7", e7, p7},
+		{"8", e8, p8},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.ea.parse(tt.pattern); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("%q: tExpectedArgs.parse() = %v, want %v",
-					tt.name, got, tt.want)
+			if tt.eo.parse(tt.pattern); tt.eo.previous != tt.pattern {
+				t.Errorf("%q: tExpectedArgs.parse() = %q, want %q",
+					tt.name, tt.eo.previous, tt.pattern)
 			}
 		})
 	}
 } // Test_tExpectedArgs_parse()
+
+/* _EoF_ */
